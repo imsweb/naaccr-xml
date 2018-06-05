1b1638b1cec629eda8e375e9c1df3751ccda3f78
depryf <depryf@imsweb.com>
2018-06-05 09:24:03 -0400

    More work to support CSV to XML

--- src/main/java/com/imsweb/naaccrxml/sas/SasCsvToXml.java
+++ src/main/java/com/imsweb/naaccrxml/sas/SasCsvToXml.java
@@ -3,14 +3,12 @@
  */
 package com.imsweb.naaccrxml.sas;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.LineNumberReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,6 +18,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 
+// TODO looks like SAS removes leading 0's of all values!
 public class SasCsvToXml {
 
     private File _csvFile, _xmlFile;
@@ -32,8 +31,6 @@ public class SasCsvToXml {
 
     public SasCsvToXml(String csvPath, String xmlPath, String naaccrVersion, String recordType) {
         _xmlFile = new File(xmlPath);
-        if (!_xmlFile.getParentFile().exists())
-            System.err.println("!!! Invalid XML path (parent folder doesn't exist): " + xmlPath);
         System.out.println(" > target XML: " + _xmlFile.getAbsolutePath());
 
         if (csvPath.endsWith(".gz"))
@@ -67,6 +64,7 @@ public class SasCsvToXml {
 
     public void convert(String fields) throws IOException {
 
+        // TODO use requested fields
         Set<String> requestedFields = null;
         if (fields != null) {
             requestedFields = new HashSet<>();
@@ -85,11 +83,11 @@ public class SasCsvToXml {
 
         }
 
-        BufferedReader reader = null;
+        LineNumberReader reader = null;
         BufferedWriter writer = null;
         try {
-            reader = new BufferedReader(new InputStreamReader(new FileInputStream(_csvFile), StandardCharsets.UTF_8));
-            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(_xmlFile), StandardCharsets.UTF_8));
+            reader = new LineNumberReader(new InputStreamReader(new FileInputStream(_csvFile), StandardCharsets.UTF_8));
+            writer = SasUtils.createWriter(_xmlFile);
 
             Pattern p = Pattern.compile(",");
 
@@ -105,22 +103,54 @@ public class SasCsvToXml {
             String line = reader.readLine();
             while (line != null) {
                 String[] valArray = p.split(line);
-                if (valArray.length != headers.size())
-                    throw new IOException("Got a row of size " + valArray.length + " but expected " + headers.size());
                 Map<String, String> values = new HashMap<>();
                 for (int i = 0; i < valArray.length; i++)
                     values.put(headers.get(i), valArray[i]);
 
+                String patNum = values.get("patientIdNumber");
+                if (patNum == null)
+                    throw new IOException("Line " + reader.getLineNumber() + ": patient ID Number is required to write XML files");
+
+                // do we have to write the root?
                 if (currentPatNum == null) {
-                    // write root
+                    writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+                    writer.write("\n");
+                    writer.write("<NaaccrData baseDictionaryUri=\"http://naaccr.org/naaccrxml/naaccr-dictionary-" + _naaccrVersion + ".xml\" recordType=\"" + _recordType
+                            + "\" xmlns=\"http://naaccr.org/naaccrxml\">\n");
+                    for (String id : rootFields) {
+                        String val = values.get(id);
+                        if (val != null && !val.trim().isEmpty())
+                            writer.write("    <Item naaccrId=\"" + id + "\">" + val + "</Item>\n");
+                    }
                 }
 
-                String patNum = values.get("patientIdNumber");
+                // do we have to write the patient?
+                if (currentPatNum == null || !currentPatNum.equals(patNum)) {
+                    if (currentPatNum != null)
+                        writer.write("    </Patient>\n");
+                    writer.write("    <Patient>\n");
+                    for (String id : patientFields) {
+                        String val = values.get(id);
+                        if (val != null && !val.trim().isEmpty())
+                            writer.write("        <Item naaccrId=\"" + id + "\">" + val + "</Item>\n");
+                    }
+                }
 
-                // TODO look at logic of tagsets and apply it here
+                // we always have to write the tumor!
+                writer.write("        <Tumor>\n");
+                for (String id : tumorFields) {
+                    String val = values.get(id);
+                    if (val != null && !val.trim().isEmpty())
+                        writer.write("            <Item naaccrId=\"" + id + "\">" + val + "</Item>\n");
+                }
+                writer.write("        </Tumor>\n");
 
+                currentPatNum = patNum;
                 line = reader.readLine();
             }
+
+            writer.write("    </Patient>\n");
+            writer.write("</NaaccrData>");
         }
         finally {
             if (writer != null)
@@ -134,4 +164,17 @@ public class SasCsvToXml {
         if (!_csvFile.delete())
             System.err.println("!!! Unable to cleanup tmp CSV file.");
     }
+
+    /**
+    public static void main(String[] args) throws IOException {
+        String xmlPath = "D:\\Users\\depryf\\dev\\synthetic-data_naaccr-18-incidence_100-recs-copy.xml";
+        String csvPath = "D:\\Users\\depryf\\dev\\synthetic-data_naaccr-18-incidence_100-recs-copy.csv";
+
+        long start = System.currentTimeMillis();
+        SasCsvToXml converter = new SasCsvToXml(csvPath, xmlPath, "180", "I");
+        converter.convert();
+        //reader.cleanup();
+        System.out.println((System.currentTimeMillis() - start) + "ms");
+    }
+     */
 }
