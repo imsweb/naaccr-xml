05bdb908d6ff47b79691cfb0250a35043d251fda
depryf <depryf@imsweb.com>
2020-07-15 12:17:12 -0400

    Added missing N21 items, review all items (#172)

---
+++ src/test/java/lab/EvaluateNaaccr21Changes.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2020 Information Management Services, Inc.
+ */
+package lab;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.Objects;
+
+import com.opencsv.CSVReader;
+
+import com.imsweb.naaccrxml.NaaccrXmlDictionaryUtils;
+import com.imsweb.naaccrxml.entity.dictionary.NaaccrDictionary;
+import com.imsweb.naaccrxml.entity.dictionary.NaaccrDictionaryItem;
+
+public class EvaluateNaaccr21Changes {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("...\\Selected fields 20200715.csv");
+
+        NaaccrDictionary dictionary = NaaccrXmlDictionaryUtils.getMergedDictionaries("210");
+
+        try (CSVReader reader = new CSVReader(new FileReader(file))) {
+            String[] line = reader.readNext(); // ignore headers
+
+            line = reader.readNext();
+            while (line != null) {
+                //if ("New".equalsIgnoreCase(line[6])) {
+                String id = line[3];
+                String num = line[0];
+                //System.out.println(id + " (length " + line[1] + ") - " + line[2]);
+
+                // error in the spreadsheet...
+                if ("phase1RadiationToDrainingLymphNodes".equals(id))
+                    id = "phase1RadiationToDrainingLN";
+
+                if (dictionary.getGroupedItemByNaaccrId(id) == null) {
+                    NaaccrDictionaryItem item = dictionary.getItemByNaaccrId(id);
+                    if (item == null)
+                        System.out.println("!!! " + id);
+                    else if (!Objects.equals(line[0], item.getNaaccrNum().toString()))
+                        System.out.println("bad num for " + num + ": " + line[0]);
+                    else if (!Objects.equals(line[1], item.getLength().toString()))
+                        System.out.println("bad length for " + num + ": " + line[1]);
+                    else if (!Objects.equals(line[2], item.getNaaccrName()))
+                        System.out.println("bad name for " + num + ": " + line[2]);
+                }
+                //}
+                line = reader.readNext();
+            }
+        }
+    }
+}
